fd9d9dff771f9daf15fb250b24a215e3038bd5d8
Bima Setyo <abimanyusrisetyo@gmail.com>
2023-03-03 07:14:51 +0700

    storage.js fixed

--- assets/storage.js
+++ assets/storage.js
@@ -48,35 +48,3 @@ function renderHistory() {
 }
  
 renderHistory();
-
-const history = {
-    firstNumber: calculator.firstNumber,
-    secondNumber: calculator.displayNumber,
-    operator: calculator.operator,
-    result: result
-}
-
-function performCalculation() {
-    if (calculator.firstNumber == null || calculator.operator == null) {
-        alert("Anda belum menetapkan operator");
-        return;
-    }
-  
-    let result = 0;
-    if (calculator.operator === "+") {
-        result = parseInt(calculator.firstNumber) + parseInt(calculator.displayNumber);
-    } else {
-        result = parseInt(calculator.firstNumber) - parseInt(calculator.displayNumber)
-    }
-  
-    // objek yang akan dikirimkan sebagai argumen fungsi putHistory()
-    const history = {
-        firstNumber: calculator.firstNumber,
-        secondNumber: calculator.displayNumber,
-        operator: calculator.operator,
-        result: result
-    }
-    putHistory(history);
-    calculator.displayNumber = result;
-    renderHistory();
- }
